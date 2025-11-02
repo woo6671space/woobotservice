4bee8f84c25a453dafb0b9ff1484da69f35abe3e
woo6671_space <woo6671.space@gmail.com>
2025-11-02 18:32:22 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'WooBot Service',
+  description: 'Made By Woo6671',
+  generator: 'WooBot Service',
 }
 
 export default function RootLayout({
